16f20c4ceb3a93085906d285729c7fc21082368f
John Kelley <johnkelley4477@gmail.com>
2017-09-02 20:38:21 -0400

    Hapi Hello World

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict'
+//Create a variable or the hapi module
+const Hapi = require('hapi');
+//Instantiate the server
+const server = new Hapi.Server();
+server.connection({port: 3000, host: 'localhost'});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request,reply){
+    reply('Hello World');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler:function(request,reply){
+    reply('Hello ' + encodeURIComponent(request.params.name) + '!');
+  }
+});
+
+server.start((err)=> {
+  if(err){
+    throw err;
+  }
+  console.log(`Server running at : ${server.info.uri}` );
+});
